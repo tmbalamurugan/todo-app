ef82f434689824d43f31559af958b52032cceabd
tmbalagan <tmbalagan@gmail.com>
2020-02-13 12:45:13 +0530

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+var http = require('http')
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
@@ -11,4 +11,9 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '/dist/crud/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+
+http.createServer(app, function(req, res){
+    res.writeHeader(200, {"Content-Type": "text/html"});  
+    res.write(html);  
+    res.end();  
+}).listen(process.env.PORT || 8080)
